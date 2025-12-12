22c4c27808a18bbffdaa887130646add8e85e297
HemlockPham7 <london1952hoang@gmail.com>
2025-12-13 05:40:06 +0700

    Created Employee

--- employeeservice/src/main/java/com/convit/employeeservice/EmployeeserviceApplication.java
+++ employeeservice/src/main/java/com/convit/employeeservice/EmployeeserviceApplication.java
@@ -2,8 +2,15 @@ package com.convit.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan({
+		"com.convit.employeeservice",
+		"com.convit.commonservice"
+})
 public class EmployeeserviceApplication {
 
 	public static void main(String[] args) {
---
+++ employeeservice/src/main/java/com/convit/employeeservice/command/aggregate/EmployeeAggregate.java
@@ -0,0 +1,39 @@
+package com.convit.employeeservice.command.aggregate;
+
+import com.convit.employeeservice.command.command.CreateEmployeeCommand;
+import com.convit.employeeservice.command.event.EmployeeCreatedEvent;
+import lombok.NoArgsConstructor;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+@Aggregate
+@NoArgsConstructor
+public class EmployeeAggregate {
+    @AggregateIdentifier
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String Kin;
+    private Boolean isDisciplined;
+
+    @CommandHandler
+    public EmployeeAggregate(CreateEmployeeCommand command){
+        EmployeeCreatedEvent event = new EmployeeCreatedEvent();
+        BeanUtils.copyProperties(command, event);
+
+        AggregateLifecycle.apply(event);
+    }
+
+    @EventSourcingHandler
+    public void on(EmployeeCreatedEvent event){
+        this.id = event.getId();
+        this.firstName = event.getFirstName();
+        this.lastName = event.getLastName();
+        this.Kin = event.getKin();
+        this.isDisciplined = event.getIsDisciplined();
+    }
+}
