8865bdc0ecbb67fb969e3bebd80a42071ed0e172
HemlockPham7 <london1952hoang@gmail.com>
2025-12-13 04:32:16 +0700

    Init commit

--- bookservice/bookservice/src/main/java/com/convit/bookservice/BookserviceApplication.java
+++ bookservice/bookservice/src/main/java/com/convit/bookservice/BookserviceApplication.java
@@ -3,9 +3,14 @@ package com.convit.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan({
+		"com.convit.bookservice",
+		"com.convit.commonservice"
+})
 public class BookserviceApplication {
 
 	public static void main(String[] args) {
--- bookservice/bookservice/src/main/java/com/convit/bookservice/command/model/BookRequestModel.java
+++ bookservice/bookservice/src/main/java/com/convit/bookservice/command/model/BookRequestModel.java
@@ -1,5 +1,8 @@
 package com.convit.bookservice.command.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class BookRequestModel {
     private String id;
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 30, message = "Name must be between 2  and 30 characters")
     private String name;
+    @NotBlank(message = "Author is mandatory")
+    @Email
     private String author;
     private Boolean isReady;
 }
